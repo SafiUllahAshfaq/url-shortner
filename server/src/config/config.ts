78801e73ec444a389d0407f9a2d0602a6e1c2701
SafiUllahAshfaq <safiu799@gmail.com>
2023-07-22 22:41:05 +0200

    feat: load config from config.ts

---
+++ server/src/config/config.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+interface AppConfig {
+  NODE_ENV: string;
+  SERVER_PORT: string;
+  DB_HOST: string;
+  DB_PORT: string;
+  DB_NAME: string;
+  BASE_URL: string;
+  REDIS_HOST: string;
+  REDIS_PORT: number;
+}
+
+// Define the expected environment variables
+const expectedEnvVars: string[] = [
+  "DB_HOST",
+  "DB_PORT",
+  "DB_NAME",
+  "BASE_URL",
+  "REDIS_HOST",
+  "REDIS_PORT",
+];
+
+// Check if all the expected environment variables are present
+for (const envVar of expectedEnvVars) {
+  if (!process.env[envVar]) {
+    throw new Error(`Required environment variable "${envVar}" is missing.`);
+  }
+}
+
+const config: AppConfig = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  SERVER_PORT: process.env.SERVER_PORT || "3009",
+  DB_HOST: process.env.DB_HOST!,
+  DB_PORT: process.env.DB_PORT!,
+  DB_NAME: process.env.DB_NAME!,
+  BASE_URL: process.env.BASE_URL!,
+  REDIS_HOST: process.env.REDIS_HOST!,
+  REDIS_PORT: parseInt(process.env.REDIS_PORT!),
+};
+
+export default config;
